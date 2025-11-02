55a9a9fcb9dc36676414a8e85333e25f7321e6f5
RoussyJesus <roussyaguilarmoran@gmail.com>
2025-11-01 22:06:26 -0500

    first commit

---
+++ header/heard.php
@@ -0,0 +1,43 @@
+<head>
+    
+    <!-- Basic page needs
+	============================================ -->
+	<title>Market - Premium Multipurpose HTML5/CSS3 Theme</title>
+	<meta charset="utf-8">
+    <meta name="keywords" content="boostrap, responsive, html5, css3, jquery, theme, multicolor, parallax, retina, business" />
+    <meta name="author" content="Magentech">
+    <meta name="robots" content="index, follow" />
+   
+	<!-- Mobile specific metas
+	============================================ -->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+	<!-- Favicon
+	============================================ -->
+    <link rel="shortcut icon" href="ico/favicon.png">
+	
+	<!-- Google web fonts
+	============================================ -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+	
+    <!-- Libs CSS
+	============================================ -->
+    <link rel="stylesheet" href="css/bootstrap/css/bootstrap.min.css">
+	<link href="css/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+	<link href="js/datetimepicker/bootstrap-datetimepicker.min.css" rel="stylesheet">
+    <link href="js/owl-carousel/owl.carousel.css" rel="stylesheet">
+	<link href="css/themecss/lib.css" rel="stylesheet">
+	<link href="js/jquery-ui/jquery-ui.min.css" rel="stylesheet">
+	<link href="js/minicolors/miniColors.css" rel="stylesheet">
+	<!-- Theme CSS
+	============================================ -->
+   	<link href="css/themecss/so_megamenu.css" rel="stylesheet">
+    <link href="css/themecss/so-categories.css" rel="stylesheet">
+	<link href="css/themecss/so-listing-tabs.css" rel="stylesheet">
+	<link href="css/header8.css" rel="stylesheet">
+	<link href="css/footer5.css" rel="stylesheet">
+	<link id="color_scheme" href="css/home8.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+	
+
+</head>
